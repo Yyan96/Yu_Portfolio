eccc67cf35007d90c13956004d7dc4605e531bfa
Yyan96 <beth_yanyu@outlook.com>
2023-09-07 18:59:50 +0100

    add slide

--- js/script.js
+++ js/script.js
@@ -1,5 +1,26 @@
-if (document.getElementById('my-work-link')) {
-  document.getElementById('my-work-link').addEventListener('click', () => {
-    document.getElementById('my-work-section').scrollIntoView({behavior: "smooth"})
-  })
-}
+document.addEventListener('DOMContentLoaded', () => {
+  if (document.getElementById('my-work-link')) {
+    document.getElementById('my-work-link').addEventListener('click', () => {
+      document.getElementById('my-work-section').scrollIntoView({ behavior: "smooth" })
+    })
+  }
+  let slideIndex = 0;
+  showSlides();
+
+  function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) { slideIndex = 1 }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+    setTimeout(showSlides, 2000); // Change image every 2 seconds
+  }
+});
